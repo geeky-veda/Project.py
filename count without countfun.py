99ff97e049be493dff1fbdb2676a18333e301198
Veda <129061546+geeky-veda@users.noreply.github.com>
2023-10-30 20:08:07 +0530

    Update count without countfun.py

--- count without countfun.py
+++ count without countfun.py
@@ -1,8 +1,8 @@
-l1=[10,20,10,200,20,30]
-n=int(input("What do u want to find repeating: "))
-count=0
+l1 = [10, 20, 10, 200, 20, 30]
+n = int(input("What do u want to find repeating: "))
+count = 0
 for i in l1:
-    if i==n:
-        count+=1
+    if i == n:
+        count += 1
 
-print(n,"is repeated",count,"times")
+print(n, "is repeated", count, "times")
